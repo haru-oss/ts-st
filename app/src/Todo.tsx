0f03fad70117e8d37c77a037eae8aac3f6b6760f
haru-oss <hauyaishungry@gmail.com>
2024-05-27 11:04:35 +0900

    Omitを使用したprops定義

--- app/src/Todo.tsx
+++ app/src/Todo.tsx
@@ -7,7 +7,7 @@ import { TodoType } from "./types/todo";
 //     completed?: boolean;
 // }
 
-export const Todo = (props:Pick<TodoType, 'userId'|'title'|'completed' >)=>{
+export const Todo = (props: Omit<TodoType, 'id' >)=>{
     const {title,userId,completed=false} = props;
     const completeMark = completed ? "[完]":"[未]"
     return(
