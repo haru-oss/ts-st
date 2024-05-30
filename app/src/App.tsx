d9bf03ad8ffb7ad10308a78cfbde30c7294ad10e
haru-oss <hauyaishungry@gmail.com>
2024-05-30 21:39:24 +0900

    カスタムフックについて学んだ

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,43 +1,31 @@
-import {  useState } from 'react';
 import './App.css'
-import axios from 'axios';
-import { Todo } from './Todo';
-import { TodoType } from './types/todo';
-import { Test } from './Test';
-import { UserProfile } from './UserProfile';
-import { User } from './types/user';
+import { UserCard } from './components/UserCard';
 
-const user:   User = {
-  name:"haruya",
-  // hobby:["pg","fittness"]
-}
+import { useAllUsers } from './hooks/useAllUsers';
 
 
 function App() {
-  const [todos,setTodos] = useState<Array<TodoType>>([])
+   const {getUsers,userProfiles,loading,error} =useAllUsers()
+
+  const onClickFetchUser =()=> getUsers();
 
 
-  const onClickFetchData = () => {
-    axios.get<Array<TodoType>>('https://jsonplaceholder.typicode.com/todos').then((res)=>{
-      setTodos(res.data);
-    });
-  };
 
   return (
     <>
-      <UserProfile user={user}/>
+        <button onClick={onClickFetchUser}>データ取得</button>
+        <br />
+        {error? (
+          <p style={{color:"red"}}>データの取得に失敗しました</p>
+        ):loading?(
+        <p>loading...</p>  ):(
+          <>
+            {userProfiles.map((user)=>
+              <UserCard key={user.id} user = {user}/>
+            )}
+          </>
+        )}
 
-      <button onClick={onClickFetchData}>データ取得</button>
-      {todos.map((todo)=>(
-        <Todo key = {todo.id}
-              // id ={todo.id}
-              title={todo.title}
-              userId={todo.userId}
-              completed ={todo.completed}
-              />
-
-      ))}
-      <Test >テストです。</Test>
 
     </>
 
--- app/src/types/user.ts
+++
@@ -1,4 +0,0 @@
-export type User = {
-    name:string;
-    hobby?:Array<string>;
-}
